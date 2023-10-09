e6e8e97622beb13c76e74322340e4485dea50e4d
Niranjan V <rathinamniranjan@gmail.com>
2023-10-10 00:26:58 +0530

    Added Random Color Generator

--- app/src/main/java/com/example/mytodolistapp/ui/fragments/fragmentUtilities/NotesRecyclerUtilities.kt
+++ app/src/main/java/com/example/mytodolistapp/ui/fragments/fragmentUtilities/NotesRecyclerUtilities.kt
@@ -1,17 +1,19 @@
 package com.example.mytodolistapp.ui.fragments.fragmentUtilities
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView.Adapter
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.mytodolistapp.data.NotesData
 import com.example.mytodolistapp.databinding.NotesListRvBinding
+import kotlin.random.Random
 
 class NotesViewHolder(private val itemBinding:NotesListRvBinding):ViewHolder(itemBinding.root) {
 
-    fun bindData(notesData: NotesData,listener: OnClickListener){
-
+    fun bindData(notesData: NotesData,listener: OnClickListener,color: Int){
         itemBinding.tvTitle.text = notesData.title
+        itemBinding.root.setCardBackgroundColor(color)
 
         itemBinding.imgDelete.setOnClickListener {
             listener.onClickDelete(notesData)
@@ -39,7 +41,28 @@ class NotesRecyclerAdapter(val listOfNotes:List<NotesData>,private val listener:
     }
 
     override fun onBindViewHolder(holder: NotesViewHolder, position: Int) {
-        holder.bindData(listOfNotes[position],listener)
+        holder.bindData(listOfNotes[position],listener,getRandomLightColorCode())
+    }
+
+    private fun getRandomLightColorCode(): Int {
+        val alpha = 255 // You can adjust the alpha value (transparency) if needed
+        val minBrightness = 192 // Adjust this value to control the minimum brightness
+
+        var red: Int
+        var green: Int
+        var blue: Int
+
+        do {
+            red = Random.nextInt(256)
+            green = Random.nextInt(256)
+            blue = Random.nextInt(256)
+        } while (calculateBrightness(red, green, blue) < minBrightness)
+
+        return Color.argb(alpha, red, green, blue)
+    }
+
+    private fun calculateBrightness(red: Int, green: Int, blue: Int): Double {
+        return (0.299 * red + 0.587 * green + 0.114 * blue)
     }
 }
 
